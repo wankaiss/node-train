a66a0c428663314b24620a941a7592f92a0aaf60
GeraldYang <578634482@qq.com>
2018-01-06 21:47:30 +0800

    add https test

--- public/javascripts/compare-utils.js
+++ public/javascripts/compare-utils.js
@@ -3,8 +3,41 @@ const fs = require('fs');
 const path = require('path');
 
 let get_https_data = function () {
+    // const options = {
+    //     hostname: 'encrypted.google.com',
+    //     port: 443,
+    //     path: '/',
+    //     method: 'GET',
+    //     // key: fs.readFileSync('test/fixtures/keys/agent2-key.pem'),
+    //     // cert: fs.readFileSync('test/fixtures/keys/agent2-cert.pem')
+    // };
+    // options.agent = new https.Agent(options);
+
+    const options = {
+        hostname: '',
+        port: 443,
+        path: '/',
+        method: 'GET'
+    };
+
+    const req = https.request(options, (res) => {
+        console.log('statusCode:', res.statusCode);
+        console.log('headers:', res.headers);
+
+        res.on('data', (d) => {
+            process.stdout.write(d);
+        });
+    });
+
+    req.on('error', (e) => {
+        console.error(e);
+    });
+    req.end();
 };
 
+/**
+ * Process wrong json data to right json data.
+ */
 let process_data = function () {
     let file_path = path.resolve('../data/source_data.json');
     // console.info(file_path);
@@ -18,4 +51,5 @@ let process_data = function () {
     fs.writeFileSync(path.resolve('../data/processed_data.json'), process_string_data);
 };
 
-process_data();
+get_https_data();
+
